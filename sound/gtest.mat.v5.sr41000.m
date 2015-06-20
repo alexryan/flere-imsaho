51b2e6463e075f02432a862949d8eee7f246a571
Alex Ryan <alexander.j.ryan@gmail.com>
2015-06-19 17:56:26 -0700

    experimentation

---
+++ sound/gtest.mat.v5.sr41000.m
@@ -0,0 +1,52 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% octave prepareTrainingData2.m
+% ls -lF test2.mat
+% octave
+% >> load('test2.mat');
+% >> size(T)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Initialization
+clear ; close all; clc
+cd test_set3
+
+song1 = loadaudio('Adele-Someone_Like_You.mono-sr41000-ss16', 'raw', 16);
+A(1,:) = song1(80001:81000,1);
+
+song2 = loadaudio('Burning_Inside.mono-sr41000-ss16', 'raw', 16);
+A(2,:) = song2(80001:81000,1);
+
+song3 = loadaudio('Christopher_Cross-Sailing.mono-sr41000-ss16', 'raw', 16);
+A(3,:) = song3(80001:81000,1);
+
+song4 = loadaudio('Deicide-Kill_the_Christian.mono-sr41000-ss16', 'raw', 16);
+A(4,:) = song4(80001:81000,1);
+
+song5 = loadaudio('Disturbed-Down_With_The_Sickness.mono-sr41000-ss16', 'raw', 16);
+A(5,:) = song4(80001:81000,1);
+
+song6 = loadaudio('James_Blunt-Youre_Beautiful.mono-sr41000-ss16', 'raw', 16);
+A(6,:) = song6(80001:81000,1);
+
+song7 = loadaudio('John_Lennon-Imagine.mono-sr41000-ss16', 'raw', 16);
+A(7,:) = song7(80001:81000,1);
+
+song8 = loadaudio('Ministry-Primitive_Future.mono-sr41000-ss16', 'raw', 16);
+A(8,:) = song8(80001:81000,1);
+
+song8 = loadaudio('Rhiana-Diamonds.mono-sr41000-ss16', 'raw', 16);
+A(9,:) = song8(80001:81000,1);
+
+song8 = loadaudio('Sony_Hackers-you_are_an_idiot.mono-sr41000-ss16', 'raw', 16);
+A(10,:) = song8(80001:81000,1);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 0 = low intensity
+% 1 = high intensity
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+b = [0; 1; 0; 1; 1; 0; 0; 1; 0; 1];
+
+cd ..
+save 'test3.mat' A b
+
---
+++ sound/test_set3/convert2raw.v5.sr41000.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#sox 001-A_Thousand_Years.mp3 -c 1 -r 41000 --bits 16 001-A_Thousand_Years.mono-sr41000-ss16.raw
+
+#ffmpeg -i Adele-Someone_Like_You.m4a Adele-Someone_Like_You.mp3
+source="Adele-Someone_Like_You.mp3"
+target="Adele-Someone_Like_You.mono-sr41000-ss16.raw"
+sox $source -c 1 -r 41000 --bits 16 $target
+
+source="Burning_Inside.mp3"
+target="Burning_Inside.mono-sr41000-ss16.raw"
+sox $source -c 1 -r 41000 --bits 16 $target
+
+source="Christopher_Cross-Sailing.mp3"
+target="Christopher_Cross-Sailing.mono-sr41000-ss16.raw"
+sox $source -c 1 -r 41000 --bits 16 $target
+
+source="Deicide-Kill_the_Christian.mp3"
+target="Deicide-Kill_the_Christian.mono-sr41000-ss16.raw"
+sox $source -c 1 -r 41000 --bits 16 $target
+
+source="Disturbed-Down_With_The_Sickness.mp3"
+target="Disturbed-Down_With_The_Sickness.mono-sr41000-ss16.raw"
+sox $source -c 1 -r 41000 --bits 16 $target
+
+#ffmpeg -i James_Blunt-Youre_Beautiful.m4a James_Blunt-Youre_Beautiful.mp3
+source="James_Blunt-Youre_Beautiful.mp3"
+target="James_Blunt-Youre_Beautiful.mono-sr41000-ss16.raw"
+sox $source -c 1 -r 41000 --bits 16 $target
+
+source="John_Lennon-Imagine.mp3"
+target="John_Lennon-Imagine.mono-sr41000-ss16.raw"
+sox $source -c 1 -r 41000 --bits 16 $target
+
+#ffmpeg -i Ministry-Primitive_Future.m4a Ministry-Primitive_Future.mp3
+source="Ministry-Primitive_Future.mp3"
+target="Ministry-Primitive_Future.mono-sr41000-ss16.raw"
+sox $source -c 1 -r 41000 --bits 16 $target
+
+source="Rhiana-Diamonds.mp3"
+target="Rhiana-Diamonds.mono-sr41000-ss16.raw"
+sox $source -c 1 -r 41000 --bits 16 $target
+
+source="Sony_Hackers-you_are_an_idiot.mp3"
+target="Sony_Hackers-you_are_an_idiot.mono-sr41000-ss16.raw"
+sox $source -c 1 -r 41000 --bits 16 $target
+
+
+ls -ltF *ss16.raw
+
+
