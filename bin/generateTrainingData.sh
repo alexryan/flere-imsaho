beb794e82db44eaeae867fd20523091b38312fa5
Alex Ryan <alexander.j.ryan@gmail.com>
2015-06-24 13:39:46 -0700

    reorganized directory struture

---
+++ bin/generateTrainingData.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Convert all m4a files to mp3 files. Delete the m4a files.
+
+
+# Convert all mp3 files to raw format.
+
+
+# Extract multiple short snippets from each raw file
+# These snippets must be long enough that a human can listen to and
+# judge its intensity or valence relative to another snippet
+
+
+# load all of these snippets into an iTunes playlist
+# where a human will manually order them from low to high intensity
+
+
+# extract the manually sorted iTunes playlist
+# generate I-numbers from 0 to 1 for all the songs in the training set.
+
+
+# randomly separate the data into training and test data
+# $ALPINE_GIT/machineLearning/sound/training
+# $ALPINE_GIT/machineLearning/sound/test
+
+# load all the data into octave
+# the raw data for the song snippets go into da matrix
+# the I-number labels go into da vector
+
+
+# invoke da magical machine learning algorithm to give birth to a magical classifier
+
+
+# now we can use da magical classifier to make prediction on test data. :)
+
+
