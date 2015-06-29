a9235e12c8f42ab629f144241413a185c4c6e264
Alex Ryan <alexander.j.ryan@gmail.com>
2015-06-29 13:07:52 -0700

    randomly assign song clips to training or test sets

--- src/generate-training-matrices.m
+++ src/generate-training-matrices.m
@@ -38,10 +38,14 @@
 % cd $FLERE_IMSAHO/data/matlab
 % ls -lF experiment-train.mat
 % octave
-% >> load('experiment-train.mat');
+% >> load('experiment-training');
 % >> size(X)
 % >> size(y)
 % >> plot(X(24,400:600))
+% >> load('experiment-test');
+% >> size(A)
+% >> size(b)
+% >> plot(A(24,400:600))
 %
 % If you see stuff, it probably worked.
 %
@@ -67,14 +71,14 @@ printf("path2RawSongFiles=%s\n", path2RawSongFiles);
 %   Pantera-Fucking_Hostile.50,1
 %   Pantera-Fucking_Hostile.60,1
 % Read the name of the audio file in the cell array "songs".
-% Read the label into the vector "y".
+% Read the label into the vector "labels".
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[songs, y] = textread(csvFile, "%s %f", "delimiter", ",");
+[songs, labels] = textread(csvFile, "%s %f", "delimiter", ",");
 
 % Did it work?
 printf("contesnts of %s\n", csvFile);
 disp(songs)
-disp(y)
+disp(labels)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Now the labels have been read into the vector y.
@@ -82,7 +86,10 @@ disp(y)
 % and injected into the matrix "X".
 % Do that now.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+trainingIndex = 1;
+testIndex = 1;
 for i = 1:length(songs)
+
   printf("i=%d\n", i);
   printf("song=%s\n", songs{i,1});
 
@@ -93,25 +100,42 @@ for i = 1:length(songs)
   printf("raw=%s\n", raw);
 
   songVector = loadaudio(raw, 'raw', 16);
-  X(i,:) = songVector(1:1000,1);
+
+  randomNumber = randi([1e1 1e2],1,1)
+
+  if (randomNumber < 80)
+    printf("%d goes to the training set\n", i);
+    X(trainingIndex,:) = songVector(1:1000,1);
+    y(trainingIndex) = labels(i);
+    trainingIndex = trainingIndex + 1;
+  else
+    printf("%d goes to the test set\n", i);
+    A(testIndex,:) = songVector(1:1000,1);
+    b(testIndex) = labels(i);
+    testIndex = testIndex + 1;
+  endif
+
 end
 
 % Did it work? Size matters! 
-printf("SIZE:\n")
+printf("size of training set:\n")
 disp(size(X))
+printf("size of test set:\n")
+disp(size(A))
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% y: label vector
-% 0 = low intensity
-% 1 = high intensity
-% Save the data to a matlab file.
+% Persist the traiing data and test dat to matlab files.
 % Example:
-%   save '/Users/alexryan/alpine/git/machineLearning/data/matlab/output1.mat' X y
+%   save '/Users/alexryan/alpine/git/flere-imhaso/data/matlab/experiment-training.mat' X y
+%   save '/Users/alexryan/alpine/git/flere-imhaso/data/matlab/experiment-test.mat' X y
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-matlabFile = fullfile(getenv("FLERE_IMSAHO"), "data/matlab", "experiment-training.mat")
-disp(matlabFile)
-save(matlabFile, 'X', 'y')
+matlabTrainingFile = fullfile(getenv("FLERE_IMSAHO"), "data/matlab", "experiment-training.mat")
+disp(matlabTrainingFile)
+save(matlabTrainingFile, 'X', 'y')
 
+matlabTestFile = fullfile(getenv("FLERE_IMSAHO"), "data/matlab", "experiment-test.mat")
+disp(matlabTestFile)
+save(matlabTestFile, 'A', 'b')
 
